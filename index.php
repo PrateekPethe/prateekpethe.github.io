e6994d4687d93d5781647bb9bc29df4dd17cbdeb
PrateekPethe <46239017+PrateekPethe@users.noreply.github.com>
2023-05-30 17:02:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,49 +1,90 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>My Website</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+  <title>My Portfolio</title>
+  <!-- Add Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
+  <style>
+    /* Add your own custom CSS styles here */
+
+    /* Example styling for the header and navigation */
+    header {
+      background-color: #333;
+      color: #fff;
+      padding: 20px;
+    }
+
+    h1 {
+      margin: 0;
+    }
+
+    nav ul {
+      list-style-type: none;
+      padding: 0;
+    }
+
+    nav ul li {
+      display: inline;
+      margin-right: 10px;
+    }
+
+    nav ul li a {
+      color: #fff;
+      text-decoration: none;
+    }
+
+    /* Example styling for the sections */
+    section {
+      padding: 50px 0;
+    }
+
+    h2 {
+      margin-bottom: 30px;
+    }
+  </style>
 </head>
 <body>
-	<header>
-		<h1>Welcome to my website</h1>
-		<nav>
-			<ul>
-				<li><a href="#about">About</a></li>
-				<li><a href="#services">Services</a></li>
-				<li><a href="#contact">Contact</a></li>
-			</ul>
-		</nav>
-	</header>
-	<main>
-		<section id="about">
-			<h2>About us</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, magna sit amet pulvinar iaculis, nisi odio tristique leo, in euismod augue magna vel dolor.</p>
-		</section>
-		<section id="services">
-			<h2>Our services</h2>
-			<ul>
-				<li>Service 1</li>
-				<li>Service 2</li>
-				<li>Service 3</li>
-			</ul>
-		</section>
-		<section id="contact">
-			<h2>Contact us</h2>
-			<form>
-				<label for="name">Name:</label>
-				<input type="text" id="name" name="name"><br><br>
-				<label for="email">Email:</label>
-				<input type="email" id="email" name="email"><br><br>
-				<label for="message">Message:</label>
-				<textarea id="message" name="message"></textarea><br><br>
-				<input type="submit" value="Submit">
-			</form>
-		</section>
-	</main>
-	<footer>
-		<p>&copy; My Website 2023</p>
-	</footer>
-	<script src="script.js"></script>
+  <header>
+    <div class="container">
+      <h1>My Portfolio</h1>
+      <nav>
+        <ul>
+          <li><a href="#resume">Resume</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+  <section id="resume">
+    <div class="container">
+      <h2>Resume</h2>
+      <!-- Insert your resume content here -->
+    </div>
+  </section>
+
+  <section id="projects">
+    <div class="container">
+      <h2>Projects</h2>
+      <!-- Insert your project details and descriptions here -->
+    </div>
+  </section>
+
+  <section id="contact">
+    <div class="container">
+      <h2>Contact</h2>
+      <!-- Insert your contact information and any contact form if desired -->
+    </div>
+  </section>
+
+  <footer>
+    <div class="container">
+      <!-- Add your footer content here -->
+    </div>
+  </footer>
+
+  <!-- Add Bootstrap JS (optional) -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
